4e44ba72eb1d9d46dd20c8df70668480bff27153
Gitea <gitea@fake.local>
2022-01-17 10:10:19 +0200

    delete aold profilers

--- src/core/ioc/libraryProfilers/typeormDbProfiler.ts
+++
@@ -1,9 +0,0 @@
-import BaseLauncher from '../BaseLauncher';
-import { Connection, ConnectionOptions, createConnection } from 'typeorm';
-
-const typeormDbProfiler = async (launcher: BaseLauncher, options: ConnectionOptions) => {
-  const connection = await createConnection(options);
-  launcher.container.bind<Connection>(Connection).toDynamicValue(() => connection);
-};
-
-export default typeormDbProfiler;
